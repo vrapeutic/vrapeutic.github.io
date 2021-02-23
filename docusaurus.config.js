8fb5d4997159f9c60b8cb50aaa923c23fb26cb7c
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-02-23 16:18:38 -0500

    initial docusaurus docs project

---
+++ docusaurus.config.js
@@ -0,0 +1,114 @@
+module.exports = {
+  title: 'VRapeutic Docs',
+  tagline: 'Virtual Worls, Real Hope.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'vrapeutic', // Usually your GitHub org/user name.
+  projectName: 'vrapeutic-docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'VRapeutic Docs',
+      logo: {
+        alt: 'VRapeutic Logo',
+        src: 'img/vrapeutic-logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          to: 'blog', 
+          label: 'Blog', 
+          position: 'left'
+        },
+        {
+          href: 'https://myvrapeutic.com/',
+          label: 'VRapeutic',
+          position: 'right',
+        },
+        {
+          href: 'http://yuram.tech/',
+          label: 'Yuram',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} VRapeutic, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
