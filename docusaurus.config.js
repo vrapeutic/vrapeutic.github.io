1585344e88d91ee9a337220bb9e994dbe510335e
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-02-24 00:32:36 -0500

    change tab logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/vrapeutic-logo.ico',
   organizationName: 'vrapeutic', // Usually your GitHub org/user name.
   projectName: 'vrapeutic-docs', // Usually your repo name.
   themeConfig: {
