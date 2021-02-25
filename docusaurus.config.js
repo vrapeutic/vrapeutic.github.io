f1eec9548a1ed9196a51152d4a231c9d47de02ef
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-02-25 00:20:07 -0500

    update logo and section names

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
       title: 'VRapeutic',
       logo: {
         alt: 'VRapeutic Logo',
-        src: 'img/vrapeutic-logo.svg',
+        src: 'img/vrapeutic-logo.ico',
       },
       items: [
         {
@@ -46,12 +46,12 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Style Guide',
+              label: 'WebXR',
               to: 'docs/',
             },
             {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
+              label: "Therapeutic Modules' Library",
+              to: 'docs/ellie-tale/',
             },
           ],
         },
@@ -113,14 +113,14 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
