8cc360d9910774f5e6589459a73ba61af40d7842
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-06-11 15:32:58 -0400

    update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,38 +1,38 @@
 module.exports = {
-  someSidebar: {
-    WebXR: [
-      'intro', 
-      'installation-guide',
-      'get-started',
-      'module-design',
-      // 'doc1'
-    ],
-    "Therapeutic Modules' Library": [
-      // 'ellie-tale',
-      {
-        'Ellie Tale': [
-          'ellie-tale-vr-module-description',
-          'ellie-tale-test-cases'
+    someSidebar: {
+        WebXR: [
+            'intro',
+            'installation-guide',
+            'get-started',
+            'module-design',
+            // 'doc1'
+        ],
+        "Therapeutic Modules' Library": [
+            // 'ellie-tale',
+            {
+                'Ellie Tale': [
+                    // 'ellie-tale-vr-module-description',
+                    'ellie-tale-test-cases'
+                ]
+            },
+            {
+                'GardenDo': [
+                    // 'garden-do-vr-module-description',
+                    'garden-do-test-cases'
+                ]
+            },
+            {
+                'Archeeko': [
+                    // 'archeeko-vr-module-description',
+                    'archeeko-test-cases'
+                ]
+            },
+            {
+                'Rodja': [
+                    // 'rodja-vr-module-description',
+                    'rodja-test-cases'
+                ]
+            }
         ]
-      },
-      {
-        'GardenDo': [
-          'garden-do-vr-module-description',
-          'garden-do-test-cases'
-        ]
-      },
-      {
-        'Archeeko': [
-          'archeeko-vr-module-description',
-          'archeeko-test-cases'
-        ]
-      },
-      {
-        'Rodja': [
-          'rodja-vr-module-description',
-          'rodja-test-cases'
-        ]
-      }
-    ]
-  },
+    },
 };
